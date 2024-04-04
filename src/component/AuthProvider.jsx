22faabf596bfc07b2687973dd020cecd1237cf39
Noman Mia <nomanislamnishad@gmail.com>
2024-04-04 17:03:07 +0600

    added google login

--- src/component/AuthProvider.jsx
+++ src/component/AuthProvider.jsx
@@ -1,28 +1,30 @@
 import PropTypes from 'prop-types';
 import {createContext, useState} from 'react';
-import {createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword,GoogleAuthProvider, signInWithPopup} from "firebase/auth";
 import auth from '../Firebase/Firebase.init';
 
 export const authContext = createContext(null)
-
+const googleProvider = new GoogleAuthProvider();
 const AuthProvider = ({children}) => {
 const [user, setUser] = useState(null)
+
 const registerUser = (email,password)=> {
    return createUserWithEmailAndPassword(auth, email, password)
    
 }
-
 const loginUser = (email,password)=> {
   return signInWithEmailAndPassword(auth, email, password)
     
 }
-
+const googleLogin = () =>{
+    return signInWithPopup(auth, googleProvider)
+}
 const authInfo ={
     registerUser,
     loginUser,
     user,
-    setUser
-    
+    setUser,
+    googleLogin
 }
     return (
         <div>
--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -3,19 +3,27 @@ import { authContext } from "./AuthProvider";
 
 
 const Login = () => {
-    const {loginUser} = useContext(authContext)
-    console.log(loginUser);
+    const {loginUser,googleLogin,setUser} = useContext(authContext)
+    
     const handleLogin = (e) => {
         e.preventDefault();
       const email = e.target.email.value;
       const password = e.target.password.value;
       console.log(email,password);
+
       //set
       loginUser(email,password)
     }
+    const handleGoogleSignin =()=>{
+        googleLogin()
+        .then(result => {
+          setUser(result.user);
+        })
+       .catch(error => console.error(error))
+    }
     return (
-        <div>
-             <form onSubmit={handleLogin} className="w-[500px] mt-14 mx-auto"> 
+        <div className=""> 
+             <form onSubmit={handleLogin} className="w-[500px] mt-14 mx-auto border p-4 rounded-3xl"> 
       <label className="input input-bordered flex items-center gap-2">
         <input type="text" className="grow" placeholder="Email" name="email" />
       </label>
@@ -23,7 +31,11 @@ const Login = () => {
         <input type="text" className="grow" placeholder="Password" name="password" />
       </label>
       <button className="btn btn-primary w-full">Login</button>
+      <div className="text-center mt-2">
+     <button onClick={handleGoogleSignin} className="btn btn-primary">Google Sign-in</button>
+     </div>
       </form>
+     
         </div>
     );
 };
